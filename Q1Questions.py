1de0b1b642deb57b89869a16a7563c0ef58a2f30
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-05-12 14:39:30 +0000

    finished

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,22 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+
+#Ask for all the inputs
+multipleChoice = input("Please Input your Multiple Choice Question: \n")
+answerA = input("Input answer for choice A: \n")
+answerB = input("Input answer for choice B: \n" )
+answerC = input("Input answer for choice C: \n")
+answerD = input("Input answer for choice D: \n")
+correct = input("Input Letter for Correct Answer: \n")
+
+#create the file handle
+questions = open("questions.txt","w")
+#write all the inputs to the file
+questions.write(multipleChoice + "\n")
+questions.write(answerA + "\n")
+questions.write(answerB + "\n")
+questions.write(answerC + "\n")
+questions.write(answerD + "\n")
+questions.write(correct)
+#close the file
+questions.close()
--- Q2Quiz.py
+++ Q2Quiz.py
@@ -1,2 +1,24 @@
 # Create a second program that will read the file questions.txt, formatted as described above, and pose the questions to the user. 
 # The program will keep score of the number of questions answered correctly.
+
+#read file
+readQ = open("questions.txt","r")
+
+question = readQ.readline()
+print("The question is:\n" + question)
+a = readQ.readline()
+b = readQ.readline()
+c = readQ.readline()
+d = readQ.readline()
+answer = readQ.readline()
+print("A." + a)
+print("B." + b)
+print("C." + c)
+print("D." + d)
+
+UserAnswer = input("What is the correct answer?\n")
+
+if UserAnswer == answer:
+    print("Correct!")
+else:
+    print("Wrong!")
